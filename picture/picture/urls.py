a643ff0bd558fa0a642786cca89dbb52a6b95213
Yao9188 <robertadonsou@gmail.com>
2022-03-13 13:23:03 +0000

    panel admin

--- picture/picture/urls.py
+++ picture/picture/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path, include
 from django.contrib import admin
+from django.conf import settings             # add this
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path(r'^', include('api.urls')),
-    path(r'^admin/', admin.site.urls)
+    path(r'^admin/', admin.site.urls),
+    path(r'^', include('api.urls'))
+    
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
